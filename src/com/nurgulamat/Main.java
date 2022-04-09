d3b030db96f8a3cc2f7d11e772405121d746fb18
nurgul212 <90399606+nurgul212@users.noreply.github.com>
2022-04-09 16:20:01 -0600

    added conditions

--- src/com/nurgulamat/Main.java
+++ src/com/nurgulamat/Main.java
@@ -6,22 +6,40 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	final byte MONTHS_IN_YEAR = 12;
-    final byte PERCENT = 100;
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
 
-    Scanner scanner = new Scanner(System.in);
+        int principle = 0;
+        float monthlyInterest = 0;
+        int numberOfPayments = 0;
 
-        System.out.print("Principle: ");
-        int principle = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
-
+        while(true){
+            System.out.print("Principle: ");
+             principle = scanner.nextInt();
+            if(principle >= 1000 && principle <= 1_000_000)
+                break;
+            System.out.println("Enter a value between 1000 and 1M");
+        }
+        while(true){
+            System.out.print("Annual Interest Rate: ");
+            float annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <=30) {
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
+        while(true){
+            System.out.print("Period (Years): ");
+            byte years = scanner.nextByte();
+            if (years >= 1 && years <= 30){
+                numberOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
         double mortgage = principle * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
 
