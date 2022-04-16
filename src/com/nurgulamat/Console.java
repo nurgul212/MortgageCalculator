2acde87ef691252925f01fb4adcba493e2648022
nurgul212 <90399606+nurgul212@users.noreply.github.com>
2022-04-16 11:02:11 -0600

    re-refactor again

--- src/com/nurgulamat/Console.java
+++ src/com/nurgulamat/Console.java
@@ -3,17 +3,12 @@ package com.nurgulamat;
 import java.util.Scanner;
 
 public class Console {
-    private static Scanner scanner = new Scanner(System.in);
-    public static double readNumber(String prompt){
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextDouble();
-    }
-    public static double readNumber(String prompt, double min, double max){
+    public static double readNumber(String prompt, double min, double max) {
         Scanner scanner = new Scanner(System.in);
         double value;
-        while(true){
+        while (true) {
             System.out.print(prompt);
-            value = scanner.nextDouble();
+            value = scanner.nextFloat();
             if (value >= min && value <= max)
                 break;
             System.out.println("Enter a value between " + min + " and " + max);
