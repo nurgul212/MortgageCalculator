86a246292edb4e7446cb8922135fd705ee1da0f6
nurgul212 <90399606+nurgul212@users.noreply.github.com>
2022-04-16 11:38:32 -0600

    update

--- src/com/nurgulamat/MortgageCalculator.java
+++ src/com/nurgulamat/MortgageCalculator.java
@@ -1,12 +1,17 @@
 package com.nurgulamat;
 
 public class MortgageCalculator {
-    public static double calculateBalance(
-            int principal,
-            float annualInterest,
-            byte years,
-            short numberOfPaymentsMade
-    ) {
+    private int principal;
+    private float annualInterest;
+    private byte years;
+
+    public MortgageCalculator(int principal, float annualInterest, byte years) {
+        this.principal = principal;
+        this.annualInterest = annualInterest;
+        this.years = years;
+    }
+
+    public double calculateBalance(short numberOfPaymentsMade) {
         float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
         float numberOfPayments = years * Main.MONTHS_IN_YEAR;
 
@@ -17,10 +22,7 @@ public class MortgageCalculator {
         return balance;
     }
 
-    public static double calculateMortgage(
-            int principal,
-            float annualInterest,
-            byte years) {
+    public double calculateMortgage() {
 
         float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
         float numberOfPayments = years * Main.MONTHS_IN_YEAR;
@@ -30,5 +32,9 @@ public class MortgageCalculator {
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
 
         return mortgage;
+    }
+
+    public short getYears() {
+        return years;
     }
 }
